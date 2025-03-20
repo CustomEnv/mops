93603f0cbd46558535e0fd4108103054760e3184
VladimirPodolian <vladimir.podolyan64@gmail.com>
2025-03-20 21:43:49 +0100

    Unused PO removed

--- tests/adata/pages/colored_blocks_page.py
+++ tests/adata/pages/colored_blocks_page.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from typing import List
 
 from mops.base.element import Element
@@ -26,17 +24,6 @@ class ColoredBlocksPage(Page):
     row2 = Row('row-2', name='second blocks row')
     card = Element('.card', name='any colored block')
     navbar = Element('.navbar', name='navbar')
-    open_popup_button = Element('#open-popup-button', name='open popup button')
 
     def get_all_cards(self) -> List[Element]:
         return self.card.wait_elements_count(16).all_elements
-
-    def open_popup(self) -> OverlayPopup:
-        self.open_popup_button.scroll_into_view().click()
-        return OverlayPopup().wait_visibility()
-
-class OverlayPopup(Group):
-    def __init__(self):
-        super().__init__('#overlay-popup', name='overlay-popup')
-
-    popup_text = Element('#popup-text', name='popup text')
