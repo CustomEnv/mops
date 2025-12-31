c7689e8ef086d2f152035639919b60fb731a6c9d
Vladimir Podolyan <vladimir.podolyan64@gmail.com>
2025-12-31 12:18:37 +0100

    Android: new screens; iOS: downgrade version

--- tests/settings.py
+++ tests/settings.py
@@ -30,7 +30,7 @@ def get_ios_desired_caps():
 
     return {
         'deviceName': env.get('IOS_DEVICE_NAME') or 'iPhone 16',
-        'platformVersion': env.get('IOS_PLATFORM_VERSION') or '26.2',
+        'platformVersion': env.get('IOS_PLATFORM_VERSION') or '18.7.3',
         'udid': env.get('udid') or '',
         'automationName': 'XCUITest',
         'platformName': 'iOS',
